0f53587185e04f7d33224ae66380b39ef9ad47d1
misteliy <yannickmisteli@gmail.com>
2018-07-09 12:29:28 +0000

    Create load_mirror.R

---
+++ load_mirror.R
@@ -0,0 +1,9 @@
+uri <- "file:///I:\\Transfer\\Analytics\\Mirror\\R"
+
+#install.packages("openxlsx")
+#install.packages("openxlsx")
+
+#install.packages("openxlsx")
+#install.packages("openxlsx")
+
+#install.packages("openxlsx")
